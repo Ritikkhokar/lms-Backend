b46a4b8092a5067a2f9a8a3a04b7dac965963ac1
Ritik Khokar <khokharritik1999@gmail.com>
2026-03-29 04:49:13 -0500

    user event added

--- src/main/java/com/example/lmsProject/ServiceImpl/UserServiceImpl.java
+++ src/main/java/com/example/lmsProject/ServiceImpl/UserServiceImpl.java
@@ -1,8 +1,10 @@
 package com.example.lmsProject.ServiceImpl;
 import com.example.lmsProject.Repository.UserRepository;
 import com.example.lmsProject.dto.UserDto;
+import com.example.lmsProject.dto.UserEvent;
 import com.example.lmsProject.entity.User;
 import com.example.lmsProject.service.EmailService;
+import com.example.lmsProject.service.UserEventProducer;
 import com.example.lmsProject.service.UserService;
 import jakarta.mail.MessagingException;
 import com.example.lmsProject.exception.ResourceNotFoundException;
@@ -11,6 +13,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -21,10 +24,12 @@ public class UserServiceImpl implements UserService {
     private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
     private final UserRepository userRepository;
     private final EmailService emailService;
+    private final UserEventProducer userEventProducer;
 
-    public UserServiceImpl(UserRepository repo, EmailService emailService) {
+    public UserServiceImpl(UserRepository repo, EmailService emailService, UserEventProducer userEventProducer) {
         this.userRepository = repo;
         this.emailService = emailService;
+        this.userEventProducer = userEventProducer;
     }
 
     @Override
@@ -51,6 +56,17 @@ public class UserServiceImpl implements UserService {
                 createdUser.getEmail(), createdUser.getEmail(), createdUser.getPasswordHash(), createdUser.getFullName()
         );
         logger.info("Created new user with id {}, evicted user caches", createdUser.getUserId());
+        UserEvent event = new UserEvent(
+                "USER_CREATED",
+                createdUser.getUserId(),
+                createdUser.getFullName(),
+                createdUser.getEmail(),
+                createdUser.getRole().getRoleName(),
+                LocalDateTime.now()
+        );
+        userEventProducer.publishUserEvent(event);
+
+        logger.info("Created user {}, published USER_CREATED event", createdUser.getUserId());
         return createdUser;
     }
 
@@ -78,6 +94,16 @@ public class UserServiceImpl implements UserService {
                 existingUser.getEmail(), existingUser.getEmail(), existingUser.getPasswordHash(), existingUser.getFullName()
         );
         logger.info("Updated user with id {}, evicted user caches", id);
+
+        UserEvent event = new UserEvent(
+                "USER_UPDATED",
+                updated.getUserId(),
+                updated.getFullName(),
+                updated.getEmail(),
+                updated.getRole().getRoleName(),
+                LocalDateTime.now()
+        );
+        userEventProducer.publishUserEvent(event);
         return updated;
     }
 
@@ -89,6 +115,18 @@ public class UserServiceImpl implements UserService {
         }
         userRepository.deleteById(id);
         logger.info("Delete user with id {}, evicted user caches", id);
+
+        UserEvent event = new UserEvent(
+                "USER_DELETED",
+                id,
+                null,
+                null,
+                null,
+                LocalDateTime.now()
+        );
+        userEventProducer.publishUserEvent(event);
+
+        logger.info("Deleted user {}, published USER_DELETED event", id);
     }
 
     @Override
